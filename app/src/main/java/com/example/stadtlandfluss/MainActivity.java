31fa1ad99f0468dea9b07ec9852a275433ccfd57
BuildTools <unconfigured@null.spigotmc.org>
2021-07-26 11:15:21 +0200

    Initial commit

--- app/src/main/java/com/example/stadtlandfluss/MainActivity.java
+++ app/src/main/java/com/example/stadtlandfluss/MainActivity.java
@@ -2,27 +2,58 @@ package com.example.stadtlandfluss;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Button
     Button rdmbtn;
     Button clear;
+    Button finish;
+
+    // TextView
     TextView rdmout;
 
+    // EditText
+    EditText stadt;
+    EditText land;
+    EditText fluss;
+
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(Configuration.ORIENTATION_PORTRAIT);
 
+        // Button
         rdmbtn = findViewById(R.id.rdm_btn);
         clear = findViewById(R.id.clear_btn);
+        finish = findViewById(R.id.finish_btn);
+
+        // TextView
         rdmout = findViewById(R.id.rdmletterout);
 
+        // EditText
+        stadt = findViewById(R.id.txt_stadt);
+        land = findViewById(R.id.txt_land);
+        fluss = findViewById(R.id.txt_fluss);
+
         rdmbtn.setOnClickListener(v -> {
+            stadt.setText("");
+            land.setText("");
+            fluss.setText("");
+
+            if (!stadt.isEnabled() && !land.isEnabled() && !fluss.isEnabled()) {
+                stadt.setEnabled(true);
+                land.setEnabled(true);
+                fluss.setEnabled(true);
+            }
 
             String[] chars = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
             rdmout.setText("Buchstabe: " + chars[(int) (Math.random() * 10)].toUpperCase());
@@ -31,9 +62,23 @@ public class MainActivity extends AppCompatActivity {
         });
 
         clear.setOnClickListener(v -> {
+            if (!stadt.isEnabled() && !land.isEnabled() && !fluss.isEnabled()) {
+                stadt.setEnabled(true);
+                land.setEnabled(true);
+                fluss.setEnabled(true);
+            }
+
             rdmout.setText("Buchstabe:");
             rdmbtn.setText("Zufallsbuchstabe");
-            Toast.makeText(getApplicationContext(), "Cleared", Toast.LENGTH_SHORT).show();
+            stadt.setText("");
+            land.setText("");
+            fluss.setText("");
+        });
+
+        finish.setOnClickListener(v -> {
+            stadt.setEnabled(false);
+            land.setEnabled(false);
+            fluss.setEnabled(false);
         });
     }
 }
